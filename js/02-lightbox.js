9ea401fbba0b011bf4ec4c3f95ea082f0dec1226
Paul Derkach <mr.derkach.55@gmail.com>
2023-08-23 13:10:31 +0300

    finished task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+document.addEventListener('DOMContentLoaded', function () {
+  const gallery = document.querySelector('.gallery');
+
+  function createGalleryItem(item) {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+      </li>
+    `;
+  }
+
+  const galleryHTML = galleryItems.map(createGalleryItem).join('');
+
+  gallery.insertAdjacentHTML('beforeend', galleryHTML);
+
+  const lightbox = new SimpleLightbox('.gallery a');
+});
